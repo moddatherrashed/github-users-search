2de2a9773b14941537edd7ea75fb0aba7751f461
moddatherrashed <moddather.developer@gmail.com>
2018-08-08 02:15:18 +0200

    design the search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,43 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Image, TextInput, StyleSheet } from 'react-native'
 
 
 const SearchBar = (props) => {
     return (
-        <View><Text>search bar</Text></View>
+        <View style={styles.searchBarContainer}>
+            <Image
+                source={require('../assets/searchIcon.png')}
+                style={styles.searchIconImage}
+                resizeMode="contain"
+            />
+            <TextInput
+                style={styles.searchInput}
+                onChangeText={props.onChangeText}
+                placeholder="Search for..."
+                underlineColorAndroid='transparent'
+                value={props.value}
+            />
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    searchBarContainer: {
+        flexDirection: 'row',
+        margin: 30,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        padding: 5,
+        elevation: 5
+    },
+    searchIconImage: {
+        height: 24,
+        width: 24,
+        flex: 0.5
+    },
+    searchInput: {
+        flex: 3.5
+    }
+})
 
 export { SearchBar }
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { SearchBar } from '../components'
 
 class SearchScreen extends Component {
     state = {}
     render() {
         return (
             <View>
-                <Text>here is search screen</Text>
+                <SearchBar
+                    onChangeText={this.handleInputChange}
+                    value={this.state.query}
+                />
             </View>
         );
     }
 }
 
-export default SearchScreen;
+export { SearchScreen }
