c30949e069b69dbe3503f7be350314afb390b68e
moddatherrashed <moddather.developer@gmail.com>
2018-08-08 02:25:54 +0200

    add content to the search screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Image, Text, StyleSheet } from 'react-native'
 import { SearchBar } from '../components'
 
 class SearchScreen extends Component {
@@ -11,9 +11,33 @@ class SearchScreen extends Component {
                     onChangeText={this.handleInputChange}
                     value={this.state.query}
                 />
+                <View style={styles.screenContent}>
+                    <Image
+                        source={{ uri: 'https://spemer.com/img/works/jekyll/github.png' }}
+                        style={styles.githubImage}
+                        resizeMode='contain'
+                    />
+                    <Text style={styles.githubUsersSearchText}>GitHub Users Search</Text>
+                </View>
             </View>
-        );
+        )
     }
 }
+
+const styles = StyleSheet.create({
+    screenContent: {
+        flex: 1
+    },
+    githubImage: {
+        height: 80
+    },
+    githubUsersSearchText: {
+        fontSize: 24,
+        color: '#4078c0',
+        fontWeight: '700',
+        margin: 15,
+        alignSelf: 'center'
+    }
+})
 
 export { SearchScreen }
